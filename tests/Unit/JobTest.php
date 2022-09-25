145795c025ac07213a20088f6cda6a36e42d2775
Asep Wahyudin <asepwahyudin11@gmail.com>
2022-09-25 07:07:41 +0700

    Add Unit Test for Job Submission

---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Job;
+
+class JobTest extends TestCase
+{
+    public function getToken($jobId) 
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, '/job/' . $jobId . '/form');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        $output = curl_exec($ch); 
+
+        if (curl_errno($ch)) die(curl_error($ch));
+        curl_close($ch); 
+
+        //Get Cookie
+        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $output, $matches);
+        $cookies = array();
+        foreach($matches[1] as $item) {
+            parse_str($item, $cookie);
+            $cookies = array_merge($cookies, $cookie);
+        }
+
+        //Get CSRF Token
+        $token = "";
+        $xmlDoc = new \DomDocument();
+        $xmlDoc->loadHTML($output);
+        $searchNode = $xmlDoc->getElementsByTagName("input");
+        foreach ($searchNode as $searchNode) 
+        {
+            if ($searchNode->getAttribute('name') == '_token') {
+                $token = $searchNode->getAttribute('name');
+            }
+        }
+
+        $result = Array(
+            "cookies" => $cookies,
+            "token" => $token
+        );
+
+        return $result;
+    }
+
+    public function curlPost($data, $jobId, $cookie = null) 
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, '/job/' . $jobId . '/apply');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+
+        if (curl_errno($ch)) die(curl_error($ch));
+        curl_close($ch);
+
+        return $output;
+    }
+
+    public function getData() 
+    {
+        $data = Array(
+            'name' => 'Asep Wahyudin',
+            'email' => 'asepwahyudin@detik.com',
+            'phone_number' => '082127304712',
+            'cover_letter' => 'Hello World',
+            'link_cv' => '/cv/cv_dicoding.pdf',
+            'skills' => 'PHP, HTML, JS, Laravel, React, Golang',
+            'uploaded_time' => date("Y-m-d H:i:s")
+        );
+
+        return $data;
+    }
+
+    /**
+     * A unit test for check submission success scenario.
+     *
+     * @return void
+     */
+    public function testSubmissionSuccess()
+    {
+        // Submit Job 
+        $jobId = 10;
+        $data = $this->getData();
+        $cookies = $this->getToken($jobId);
+        $data['job_id'] = $jobId;
+        $data['_token'] = $cookies['token'];
+        $response = $this->curlPost($data, $jobId, $cookies['cookies']);
+        $isSuccess = ($response['code'] == "201");
+
+        // Check Database
+        $countData = Job::where('id', $response['last_id'])->count();
+        $isReady = ($countData > 0);
+
+        if ($countData > 0) {
+            Job::destroy($response['last_id']);
+        }
+
+        $this->assertEquals($isSuccess, $isReady, "Submission success scenario failed, data not found");
+    }
+
+    /**
+     * A unit test for check submission closed scenario.
+     *
+     * @return void
+     */
+    public function testSubmissionClosed()
+    {
+        // Submit Job 
+        $jobId = 1;
+        $data = $this->getData();
+        $cookies = $this->getToken($jobId);
+        $data['job_id'] = $jobId;
+        $data['_token'] = $cookies['token'];
+        $response = $this->curlPost($data, $jobId, $cookies['cookies']);
+        $isSuccess = ($response['code'] == "500" && $response['message'] == "Pekerjaan ini sudah tidak dapat menerima kandidat!");
+
+        $this->assertEquals($isSuccess, true, "Submission closed scenario failed, job can still receive data");
+    }
+}
